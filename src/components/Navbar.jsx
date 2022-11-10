b5cc4560fe3c739e16a221080b6173111f66ff8e
Subclic <subclic@airdesubclic2-1.home>
2022-11-10 12:06:04 +0100

    css card ok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,8 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="flex justify-center w-screen bg-gradient-to-r from-red-500 to-blue-500">
-      <Link to="/" className="flex justify-center">
-        <img
-          className="w-1/5 m-4"
-          src="./assets/pokemon.png"
-          alt="Logo Pokemon"
-        />
+      <Link to="/" className="flex justify-center w-1/6">
+        <img className="m-4" src="./assets/pokemon.png" alt="Logo Pokemon" />
       </Link>
     </div>
   );
