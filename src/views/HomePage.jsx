0effb987f10493b5ef8f76aeac7f3cd93f394436
Subclic <subclic@airdesubclic2-1.home>
2022-11-10 14:36:44 +0100

    anim loading parfait

--- src/views/HomePage.jsx
+++ src/views/HomePage.jsx
@@ -6,6 +6,10 @@ const HomePage = () => {
   const [loading, setLoading] = useState(false);
   const [count, setCount] = useState(40);
 
+  //récupération des 40 premiers poke
+  const firstPokemons = pokemons.slice(0, count);
+
+  //fetch api
   useEffect(() => {
     setLoading(true);
     fetch(`https://pokebuildapi.fr/api/v1/pokemon`)
@@ -16,6 +20,18 @@ const HomePage = () => {
       });
   }, []);
 
+  //scroll load avec délais pour test
+  useEffect(() => {
+    if (loading === true) {
+      setTimeout(() => {
+        setCount(count + 20);
+        setLoading(false);
+      }, "1000");
+    }
+    window.addEventListener("scroll", loadMore);
+    return () => window.removeEventListener("scroll", loadMore);
+  }, [count, loading]);
+
   const loadMore = () => {
     if (
       window.innerHeight + document.documentElement.scrollTop + 1 >
@@ -25,22 +41,15 @@ const HomePage = () => {
     }
   };
 
-  useEffect(() => {
-    if (loading === true) {
-      setCount(count + 20);
-
-      setLoading(false);
-    }
-    window.addEventListener("scroll", loadMore);
-    return () => window.removeEventListener("scroll", loadMore);
-  }, [count, loading]);
-
   // console.log(pokemons);
-  const firstPokemons = pokemons.slice(0, count);
 
   return (
-    <div className="bg-slate-100 w-screen min-h-screen flex flex-wrap justify-center p-4">
-      {loading && <p>... loading</p>}
+    <div className="bg-slate-100 w-screen min-h-screen flex flex-wrap justify-center p-4 relative">
+      {loading && (
+        <div role="status" className="absolute bottom-0 right-0 m-4">
+          <img className="w-20" src="./assets/DD0.gif" alt="Carapuce bg" />
+        </div>
+      )}
 
       {firstPokemons &&
         firstPokemons.map((pokemon, id) => (
