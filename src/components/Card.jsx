6f53a9e33921b39b9872be257f4cd7a94fdcb545
Subclic <subclic@airdesubclic2-1.home>
2022-11-10 15:24:41 +0100

    modif cards avec type

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,7 +10,15 @@ const Card = (props) => {
       className="flex flex-col items-center w-1/6 m-2 rounded-lg shadow transform transition duration-500 hover:scale-110"
     >
       <img src={pokemon.sprite} alt={pokemon.name} />
-      <h1>{pokemon.name}</h1>
+      <p className="text-slate-500 text-start">No.{pokemon.pokedexId}</p>
+      <p>{pokemon.name}</p>
+      <div className="flex flex-row items-left pb-2">
+        {pokemon.apiTypes.map((type, i) => (
+          <div className="w-4">
+            <img src={type.image} alt={type.name} key={i} />
+          </div>
+        ))}
+      </div>
     </button>
   );
 };
